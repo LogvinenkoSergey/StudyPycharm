1f01de71d7054a9fa6ff3c5d726cc1104ed638ce
LogvinenkoSergey <sambogat@yandex.ru>
2023-03-14 12:00:26 +0300

    4_3 main_page.py

--- Study/pages/main_page.py
+++ Study/pages/main_page.py
@@ -1,19 +1,7 @@
+
 from .base_page import BasePage
-import math
-from .locators import MainPageLocators
-from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
-
-    def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
-
-    def test_guest_should_see_login_link(browser):
-        link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)
-        page.open()
-        page.should_be_login_link()
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
